ebd8962bda99d7a80d1508d8d24bcb512671cc1e
Olga <fej-d@yandex.ru>
2018-06-03 09:59:56 +0300

    using class

--- sort.js
+++ sort.js
@@ -1,15 +1,26 @@
-'use strict';
+"use strict";
 
-(function () {
-    window.sort = {
-        sortData: function (array) {
-            for (var i = 0; i < array.length - 1; i++) {
-                if (parseInt(array[i+1].innerText) < parseInt(array[i].innerText)) {
-                    window.render.swap(array[i], array[i+1]);
-                    break;
-                }
-            }
-            return array;
+(function() {
+  class Sorter {
+    constructor(array) {
+      this.array = array;
+    }
+    next() {
+      var array = this.array;
+      for (var i = 0; i < array.length - 1; i++) {
+        if (parseInt(array[i + 1].innerText) < parseInt(array[i].innerText)) {
+          window.render.swap(array[i], array[i + 1]);
+          break;
         }
-    };
+      }
+      return array;
+    }
+  }
+
+  window.sort = {
+    sortData: function(array) {
+      var sorter = new Sorter(array);
+      return sorter.next();
+    }
+  };
 })();
