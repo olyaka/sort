2d86e85ffb1bea9e1f17749e78c8d37a3bcf7eeb
Olga <fej-d@yandex.ru>
2018-06-04 15:15:57 +0300

    fixed rendering

--- render.js
+++ render.js
@@ -1,41 +1,54 @@
 "use strict";
 
 (function() {
+  var list = document.querySelector(".list");
+
   window.render = {
     column: function(element) {
-      var list = document.querySelector(".list");
-
       var col = document.createElement("div");
       col.classList.add("column");
       col.innerText = element;
       col.style.height = element * 20 + "px";
       list.appendChild(col);
     },
-    swap: function(first, second) {
-      var swapElements = function(evt) {
-        evt.currentTarget.classList.remove("selected");
-        evt.currentTarget.nextSibling.classList.remove("selected");
-        evt.currentTarget.style.transform += "translateX(-" + 35 + "px)";
-        evt.currentTarget.nextSibling.style.transform +=
-          "translateX(" + 35 + "px)";
-        evt.currentTarget.parentNode.insertBefore(
-          evt.currentTarget.nextSibling,
-          evt.currentTarget
-        );
-        evt.currentTarget.removeEventListener("transitionend", swapElements);
-        window.script.button.addEventListener(
-          "click",
-          window.script.onButtonClick
-        );
-      };
+    swap: function(array) {
+      var divs = document.querySelectorAll(".column");
+      for (var i = 0; i < array.length; i++) {
+        if (array[i] != parseInt(divs[i].innerHTML)) {
+          var swapElements = function(evt) {
+            evt.currentTarget.removeEventListener(
+              "transitionend",
+              swapElements
+            );
+            evt.currentTarget.classList.remove("selected");
+            evt.currentTarget.previousSibling.classList.remove("selected");
 
-      first.addEventListener("transitionend", swapElements);
+            evt.currentTarget.style.transform = "translateX(-" + 0 + "px)";
+            evt.currentTarget.previousSibling.style.transform =
+              "translateX(" + 0 + "px)";
 
-      first.classList.add("selected");
-      second.classList.add("selected");
+            evt.currentTarget.parentNode.insertBefore(
+              evt.currentTarget,
+              evt.currentTarget.previousSibling
+            );
 
-      first.style.transform += "translateX(" + 35 + "px)";
-      second.style.transform += "translateX(-" + 35 + "px)";
+            window.script.button.addEventListener(
+              "click",
+              window.script.onButtonClick
+            );
+          };
+
+          divs[i + 1].addEventListener("transitionend", swapElements);
+
+          divs[i].classList.add("selected");
+          divs[i + 1].classList.add("selected");
+
+          divs[i].style.transform = "translateX(" + 35 + "px)";
+          divs[i + 1].style.transform = "translateX(-" + 35 + "px)";
+
+          break;
+        }
+      }
     }
   };
 })();
