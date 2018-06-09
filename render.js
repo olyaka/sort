590d182a604f11c95d3721492ca7259a01230ae8
olyaka <fej-d@yandex.ru>
2018-06-09 14:53:33 +0300

    div transform

--- render.js
+++ render.js
@@ -11,40 +11,29 @@
       col.style.height = element * 20 + "px";
       list.appendChild(col);
     },
-    swap: function(array) {
+    swap: function(sorter) {
       var divs = document.querySelectorAll(".column");
-      for (var i = 0; i < array.length; i++) {
-        if (array[i] != parseInt(divs[i].innerHTML)) {
-          var swapElements = function(evt) {
-            evt.currentTarget.removeEventListener(
-              "transitionend",
-              swapElements
-            );
-            evt.currentTarget.classList.remove("selected");
-            evt.currentTarget.previousSibling.classList.remove("selected");
+      for (var i = 0; i < sorter.array.length; i++) {
+        if (sorter.array[i] != parseInt(divs[i].innerHTML)) {
 
-            evt.currentTarget.style.transform = "translateX(-" + 0 + "px)";
-            evt.currentTarget.previousSibling.style.transform =
-              "translateX(" + 0 + "px)";
+          var div1 = divs[i];
+          var div2 = divs[i + 1];
 
-            evt.currentTarget.parentNode.insertBefore(
-              evt.currentTarget,
-              evt.currentTarget.previousSibling
-            );
+          div1.classList.add("selected");
+          div2.classList.add("selected");
 
-            window.script.button.addEventListener(
-              "click",
-              window.script.onButtonClick
-            );
-          };
+          div1.style.transform = "translateX(" + 35 + "px)";
+          div2.style.transform = "translateX(-" + 35 + "px)";
 
-          divs[i + 1].addEventListener("transitionend", swapElements);
+          setTimeout(function() {
+            div1.classList.remove('selected');
+            div2.classList.remove('selected');
 
-          divs[i].classList.add("selected");
-          divs[i + 1].classList.add("selected");
+            div1.style.transform = "translateX(" + 0 + "px)";
+            div2.style.transform = "translateX(" + 0 + "px)";
 
-          divs[i].style.transform = "translateX(" + 35 + "px)";
-          divs[i + 1].style.transform = "translateX(-" + 35 + "px)";
+            div1.parentNode.insertBefore(div2, div1);
+          }, 1000);
 
           break;
         }
